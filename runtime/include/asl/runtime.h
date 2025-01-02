3b883b54c21190473c68155b71904cbcbe93055c
Alastair Reid <alastair.d.reid@gmail.com>
2025-01-02 14:47:34 +0000

    Runtime: avoid using _BitInt(256) on systems that only support 128 bit ints

--- runtime/include/asl/runtime.h
+++ runtime/include/asl/runtime.h
@@ -12,6 +12,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <limits.h>
 
 // The following types are used in the foreign function interface
 typedef __int128 ASL_int_t;
@@ -20,8 +21,12 @@ typedef uint16_t ASL_bits16_t;
 typedef uint32_t ASL_bits32_t;
 typedef uint64_t ASL_bits64_t;
 typedef unsigned __int128 ASL_bits128_t;
+#if BITINT_MAXWIDTH >= 256
 typedef unsigned _BitInt(256) ASL_bits256_t;
+#endif
+#if BITINT_MAXWIDTH >= 256
 typedef unsigned _BitInt(512) ASL_bits512_t;
+#endif
 
 #define ASL_cast_bits_512_64(x) ((ASL_bits64_t)(x))
 #define ASL_cast_bits_256_64(x) ((ASL_bits64_t)(x))
