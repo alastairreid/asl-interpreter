4a6e6fc104808309a08f3cbf83e2433e3f1dbb26
Alastair Reid <alastair.d.reid@gmail.com>
2025-01-14 12:04:59 +0000

    MLIR backend: start on parameterized types

--- libASL/backend_mlir.ml
+++ libASL/backend_mlir.ml
@@ -201,55 +201,80 @@ let locals = new Asl_utils.nameSupply "%"
 let valueLit (loc : Loc.t) (fmt : PP.formatter) (x : Value.value) : unit =
   ( match x with
   | VInt v    -> PP.fprintf fmt "asl.constant_int %s {attr_dict}" (Z.to_string v)
-  | VBits v   -> PP.fprintf fmt "asl.constant_bits %s : !asl.bits<%d>{attr_dict}" (Z.to_string v.v) v.n
+  | VBits v   -> PP.fprintf fmt "asl.constant_bits %s : !asl.bits<%d> {attr_dict}" (Z.to_string v.v) v.n
   | VString v -> PP.fprintf fmt "asl.constant_string \"%s\" {attr_dict}" (String.escaped v)
   | _ -> raise (InternalError (loc, "valueLit", (fun fmt -> Value.pp_value fmt x), __LOC__))
   )
 
-let parameters (fmt : PP.formatter) (pvs : Ident.t list) : unit =
-  if List.is_empty pvs then begin
+(* Todo: the following is a hack that can only cope with a few simple kinds of expression
+ * that appear in types but this is definitely not sufficient to express all ASL types.
+ *
+ * The problem we face is that an expression like "N + 1" would normally
+ * be flattened to
+ *
+ *   %t0 = 1
+ *   %t1 = asl.add_int %N %t0
+ *
+ * and it is not clear where that sequence of statements should go in the places
+ * where types can occur
+ *)
+let simple_expr (loc : Loc.t) (fmt : PP.formatter) (x : AST.expr) : unit =
+  ( match x with
+  | Expr_Lit (VInt v) -> PP.pp_print_string fmt (Z.to_string v)
+  | Expr_Var v -> varident fmt v
+  | _ ->
+      let msg = Format.asprintf "simple_expr: overly complex expression" in
+      let pp fmt = FMT.expr fmt x in
+      raise (Error.Unimplemented (loc, msg, pp))
+  )
+
+let simple_exprs (loc : Loc.t) (fmt : PP.formatter) (xs : AST.expr list) : unit =
+  commasep (simple_expr loc) fmt xs
+
+let parameters (loc : Loc.t) (fmt : PP.formatter) (ps : AST.expr list) : unit =
+  if List.is_empty ps then begin
     ()
   end else begin
-    commasep varident fmt pvs
+    PP.fprintf fmt "<%a>"
+      (simple_exprs loc) ps
   end
 
 let rec prim_apply (loc : Loc.t) (fmt : PP.formatter) (f : Ident.t) (ps : AST.expr list) (args : AST.expr list) : Ident.t =
-  let pvs = List.map (expr loc fmt) ps in
   let avs = List.map (expr loc fmt) args in
   let fty = Identset.Bindings.find f !funtypes in
   let t = locals#fresh in
   PP.fprintf fmt "%a = %a%a %a : %a@,"
     varident t
     prim_name f
-    parameters pvs
+    (parameters loc) ps
     (commasep varident) avs
     (prim_funtype loc) fty;
   t
 
 and user_apply (loc : Loc.t) (fmt : PP.formatter) (f : Ident.t) (ps : AST.expr list) (args : AST.expr list) : Ident.t =
-  let pvs = List.map (expr loc fmt) ps in
   let avs = List.map (expr loc fmt) args in
   let fty = Identset.Bindings.find f !funtypes in
   let t = locals#fresh in
   PP.fprintf fmt "%a = asl.call @%a%a(%a) : %a@,"
     varident t
     ident f
-    parameters pvs
+    (parameters loc) ps
     (commasep varident) avs
     (funtype loc) fty;
   t
 
 and formal_param (loc : Loc.t) (fmt : PP.formatter) (x : (Ident.t * AST.ty option)) : unit =
   let (v, ot) = x in
-  PP.fprintf fmt "%a : %a"
+  (* Note: formal parameters are assumed to be integer (with no constraints) *)
+  PP.fprintf fmt "%a"
     varident v
-    (pp_type loc) (Option.value ~default:(AST.Type_Integer(None)) ot)
 
 and formal_params (loc : Loc.t) (fmt : PP.formatter) (xs : (Ident.t * AST.ty option) list) : unit =
   if List.is_empty xs then begin
     ()
   end else begin
-    commasep (formal_param loc) fmt xs
+    PP.fprintf fmt "<%a>"
+      (commasep (formal_param loc)) xs
   end
 
 and formal_arg (loc : Loc.t) (fmt : PP.formatter) (x : (Ident.t * AST.ty)) : unit =
@@ -295,6 +320,13 @@ and expr (loc : Loc.t) (fmt : PP.formatter) (x : AST.expr) : Ident.t =
         ident t
         (valueLit loc) v;
       t
+  | Expr_Slices (Type_Integer _, Expr_Lit (VInt v), [Slice_LoWd (lo, Expr_Lit (VInt wd))]) when lo = Asl_utils.zero ->
+      let t = locals#fresh in
+      PP.fprintf fmt "%a = asl.constant_bits %s : !asl.bits<%s> {attr_dict}@,"
+        ident t
+        (Z.to_string v)
+        (Z.to_string wd);
+      t
   | Expr_Var v ->
       (* todo: if v is a global, it needs to be copied into a local *)
       v
@@ -308,31 +340,6 @@ and expr (loc : Loc.t) (fmt : PP.formatter) (x : AST.expr) : Ident.t =
       let pp fmt = FMT.expr fmt x in
       raise (Error.Unimplemented (loc, "expression", pp))
   )
-
-(* Todo: the following is a hack that can only cope with a few simple kinds of expression
- * that appear in types but this is definitely not sufficient to express all ASL types.
- *
- * The problem we face is that an expression like "N + 1" would normally
- * be flattened to
- *
- *   %t0 = 1
- *   %t1 = asl.add_int %N %t0
- *
- * and it is not clear where that sequence of statements should go in the places
- * where types can occur
- *)
-and simple_expr (loc : Loc.t) (fmt : PP.formatter) (x : AST.expr) : unit =
-  ( match x with
-  | Expr_Lit v -> valueLit loc fmt v
-  | Expr_Var v -> ident fmt v
-  | _ ->
-      let msg = Format.asprintf "simple_expr: overly complex expression" in
-      let pp fmt = FMT.expr fmt x in
-      raise (Error.Unimplemented (loc, msg, pp))
-  )
-
-and simple_exprs (loc : Loc.t) (fmt : PP.formatter) (xs : AST.expr list) : unit =
-  commasep (simple_expr loc) fmt xs
 
 and constraint_range (loc : Loc.t) (fmt : PP.formatter) (x : AST.constraint_range) : unit =
   ( match x with
@@ -402,22 +409,20 @@ let decl (fmt : PP.formatter) (x : AST.stmt) : unit =
 let return_type = ref (AST.Type_Tuple [])
 
 let prim_call (loc : Loc.t) (fmt : PP.formatter) (f : Ident.t) (ps : AST.expr list) (args : AST.expr list) : unit =
-  let pvs = List.map (expr loc fmt) ps in
   let avs = List.map (expr loc fmt) args in
   let fty = Identset.Bindings.find f !funtypes in
   PP.fprintf fmt "%a%a %a : %a@,"
     prim_name f
-    parameters pvs
+    (parameters loc) ps
     (commasep varident) avs
     (prim_funtype loc) fty
 
 let user_call (loc : Loc.t) (fmt : PP.formatter) (f : Ident.t) (ps : AST.expr list) (args : AST.expr list) : unit =
-  let pvs = List.map (expr loc fmt) ps in
   let avs = List.map (expr loc fmt) args in
   let fty = Identset.Bindings.find f !funtypes in
   PP.fprintf fmt "asl.call @%a%a(%a) : %a@,"
     ident f
-    parameters pvs
+    (parameters loc) ps
     (commasep varident) avs
     (funtype loc) fty
 
