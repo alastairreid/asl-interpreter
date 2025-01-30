fce64fc53abf662536a786598cdb2cde9c457803
Alastair Reid <alastair.reid@intel.com>
2025-01-30 15:11:03 +0000

    Xform_tuples: add a isReturnType
    This provides a way to test whether a type was generated
    by the :xform_tuples transformation - which is needed
    to support tuple-return in the foreign function interface
    
    Signed-off-by: Alastair Reid <alastair.reid@intel.com>

--- libASL/xform_tuples.mli
+++ libASL/xform_tuples.mli
@@ -7,6 +7,11 @@
 
 module AST = Asl_ast
 
+(* Used by FFI support in the code generator to support
+ * import/export of functions that return tuples of results
+ *)
+val isReturnTypeName : Ident.t -> bool
+
 val xform_stmts : AST.stmt list -> AST.stmt list
 
 val xform_decls : AST.declaration list -> AST.declaration list
